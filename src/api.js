0280c1900bb6880d254e14c52f7deb376c4f23b1
muppudathiganesh <muppuraj11@gmail.com>
2025-09-22 18:11:37 +0530

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://stepup-backfootwear.onrender.com/api/contact/",
+  baseURL: "https://stepup-backfootwear.onrender.com/api/",
 });
 
 export default API;
