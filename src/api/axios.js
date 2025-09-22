53097cae1f14a2f405022a13febcc2d8c461e5ff
muppudathiganesh <muppuraj11@gmail.com>
2025-09-22 18:23:20 +0530

    Update axios.js

--- src/api/axios.js
+++ src/api/axios.js
@@ -2,15 +2,10 @@ import axios from 'axios';
 
 // Create an axios instance
 const api = axios.create({
-  baseURL: 'https://stepup-backfootwear.onrender.com/',  // Your Django API base URL
+  baseURL: 'https://stepup-backfootwear.onrender.com/api/',  // Your Django API base URL
   headers: {
     'Content-Type': 'application/json',
   },
 });
-await api.get('/api/products/women/');
-
-await api.get('/api/products/men/');
-
-await api.get('/api/products/kids/');
 
 export default api;
